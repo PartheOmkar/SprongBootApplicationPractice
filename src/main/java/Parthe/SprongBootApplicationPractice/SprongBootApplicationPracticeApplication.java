c0a05653217c9b2d973e261aaf88277414f6b594
Omkar Parthe <partheomkar362@gmail.com>
2022-12-13 19:37:20 +0530

    Project init

---
+++ src/main/java/Parthe/SprongBootApplicationPractice/SprongBootApplicationPracticeApplication.java
@@ -0,0 +1,13 @@
+package Parthe.SprongBootApplicationPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprongBootApplicationPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprongBootApplicationPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Parthe/SprongBootApplicationPractice/SprongBootApplicationPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package Parthe.SprongBootApplicationPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprongBootApplicationPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
